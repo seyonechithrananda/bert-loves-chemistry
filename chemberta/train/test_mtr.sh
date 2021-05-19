94e42f01a2177aa7242ff47cbba37c26f76e79bd
Elana Simon <elanasimon95@gmail.com>
2021-05-19 19:42:03 +0000

    made paths relative

--- chemberta/train/test_mtr.sh
+++ chemberta/train/test_mtr.sh
@@ -1,7 +1,7 @@
 python train_roberta.py \
     --model_type=regression \
-    --dataset_path=/home/ubuntu/src/bert-loves-chemistry/chemberta/data/pubchem_descriptors_sample_1k_clean.csv \
-    --normalization_path=/home/ubuntu/src/bert-loves-chemistry/chemberta/data/pubchem_descriptors_sample_1k_normalization_values.json \
+    --dataset_path=../data/pubchem_descriptors_sample_1k_clean.csv \
+    --normalization_path=../data/pubchem_descriptors_sample_1k_normalization_values.json \
     --output_dir=test_1k \
     --run_name=regression \
     --per_device_train_batch_size=8 \
